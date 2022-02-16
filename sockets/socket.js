6670faf1051c8018bdb6dabae27cf2ea11000462
desarrollop <desarrollo.phara@outlook.es>
2022-02-16 10:30:54 -0600

    Fin sección 3

---
+++ sockets/socket.js
@@ -0,0 +1,21 @@
+const {io}= require('../index');
+
+//Mensajes de sockets
+io.on('connection', client=>{
+    console.log('Cliente conectado');
+
+    client.on('disconnect', ()=>{
+        console.log('Cliente desconectado');
+    });
+
+    client.on('mensaje', (payload)=>{
+        console.log('mensaje de', payload['nombre']);
+
+        io.emit('mensaje', {admin: 'Nuevo mensaje'});
+
+    });
+
+
+});
+
+
